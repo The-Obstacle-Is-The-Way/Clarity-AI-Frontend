61011ba0a2c5711296d6b1ce407673577aa3804f
The-Obstacle-Is-The-Way <jj@novamindnyc.com>
2025-05-04 17:36:30 -0400

    feat(presentation): add clinical and patient components to the presentation layer, enhancing the organization of clinical and patient-related functionalities

---
+++ src/presentation/organisms/clinical/index.ts
@@ -0,0 +1,5 @@
+export { default as BiometricMonitorPanel } from './BiometricMonitorPanel';
+export { default as ClinicalMetricsPanel } from './ClinicalMetricsPanel';
+export { default as ClinicalRecordList } from './ClinicalRecordList';
+export { default as ClinicalTimelinePanel } from './ClinicalTimelinePanel';
+export { default as RiskAssessmentPanel } from './RiskAssessmentPanel';
---
+++ src/presentation/organisms/patient/index.ts
@@ -0,0 +1,3 @@
+export { default as PatientDetailCard } from './PatientDetailCard';
+export { default as PatientForm } from './PatientForm';
+export { default as PatientTable } from './PatientTable';
